6adde965539ac0e68311a121603ace481790a725
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-03-04 14:46:10 -0600

    Add base code for making a grid on the page

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const squaresContainer = document.querySelector(".squares-container");
+const totalSquares = 144;
+const totalMargins = 2 * 13;
+const totalBorders = 24;
+const stuffSizeTotal = totalBorders + totalMargins;
+const sideSize = (500 - stuffSizeTotal) / 12;
+const truncSide = sideSize.toFixed(3) - 2;
+
+for (let i = 0; i < totalSquares; i++) {
+  const newSqEl = document.createElement("div");
+  newSqEl.className = "square";
+  newSqEl.style.width = `${truncSide}px`;
+  newSqEl.style.height = `${truncSide}px`;
+  squaresContainer.appendChild(newSqEl);
+}
