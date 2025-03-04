7ee628b728b6f95395cbd958795a03552d56f71b
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-03-04 15:21:07 -0600

    Add logic and HTML to allow user to change grid size

--- scripts.js
+++ scripts.js
@@ -1,21 +1,54 @@
+const squaresContainer = document.querySelector(".squares-container");
+
+function calculateSquareSize(numSide) {
+  const totalSquares = numSide ** 2;
+  const totalMargins = 2 * (numSide + 1);
+  const totalBorders = 2 * numSide;
+  const stuffSizeTotal = totalBorders + totalMargins;
+  const sideSize = (500 - stuffSizeTotal) / numSide;
+  const truncSide = sideSize.toFixed(3) - 2;
+  return truncSide;
+}
+
+function drawGrid(numSquares) {
+  const squareSize = calculateSquareSize(numSquares);
+  for (let i = 0; i < numSquares ** 2; i++) {
+    const newSqEl = document.createElement("div");
+    newSqEl.className = "square";
+    newSqEl.style.width = `${squareSize}px`;
+    newSqEl.style.height = `${squareSize}px`;
+    newSqEl.addEventListener("mouseenter", handleMouseEnter);
+    squaresContainer.appendChild(newSqEl);
+  }
+}
+
+function clearGrid() {
+  while (squaresContainer.firstChild) {
+    squaresContainer.removeChild(squaresContainer.firstChild);
+  }
+}
+
 function handleMouseEnter(e) {
   const targetEl = e.target;
   targetEl.classList.add("add-color");
 }
 
-const squaresContainer = document.querySelector(".squares-container");
-const totalSquares = 144;
-const totalMargins = 2 * 13;
-const totalBorders = 24;
-const stuffSizeTotal = totalBorders + totalMargins;
-const sideSize = (500 - stuffSizeTotal) / 12;
-const truncSide = sideSize.toFixed(3) - 2;
-
-for (let i = 0; i < totalSquares; i++) {
-  const newSqEl = document.createElement("div");
-  newSqEl.className = "square";
-  newSqEl.style.width = `${truncSide}px`;
-  newSqEl.style.height = `${truncSide}px`;
-  newSqEl.addEventListener("mouseenter", handleMouseEnter);
-  squaresContainer.appendChild(newSqEl);
+function handleChangeSize() {
+  const answer = prompt("Enter number of sides for drawing grid (1 to 100):");
+  if (answer) {
+    const newSize = +answer;
+    if (newSize < 1 || newSize > 100) {
+      alert("Please enter a number between 1 and 100");
+    } else if (!Number.isInteger(newSize)) {
+      alert("Please enter an integer between 1 and 100");
+    } else {
+      clearGrid();
+      drawGrid(newSize);
+    }
+  }
 }
+
+const sizeBtnEl = document.querySelector(".size-prompt");
+sizeBtnEl.addEventListener("click", handleChangeSize);
+
+drawGrid(16);
